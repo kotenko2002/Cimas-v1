630b1119b99d509ebca58fc33a16c1ade9d96966
Dmytro Kotenko <dmytro.ko@qualer.com>
2022-12-07 10:30:04 +0200

    add enums

---
+++ Libraries/Cimas.Сommon/Enums/RepostStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cimas.Сommon.Enums
+{
+    public enum RepostStatus
+    {
+        NotReviewed = 1,
+        Approved = 2,
+        Rejected = 3
+    }
+}
---
+++ Libraries/Cimas.Сommon/Enums/SeatStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Cimas.Сommon.Enums
+{
+    public enum SeatStatus
+    {
+        Free = 1,
+        Booked = 2,
+        Occupied = 3,
+        NotAvailable
+    }
+}
